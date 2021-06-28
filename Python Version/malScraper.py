4d7fdc83ddff7d209686fe2d0a083cb06d3cde1c
Ryan Monaghan <monaghanryan95@gmail.com>
2021-06-28 22:15:21 +0100

    Imports, Data Locations, CodeLoad API,

--- Python Version/malScraper.py
+++ Python Version/malScraper.py
@@ -1,5 +1,13 @@
-from colorama import Fore, Back, Style
+import os
 import pyfiglet
+import getpass
+from time import sleep
+from datetime import datetime
+import subprocess as sp
+import importlib.util
+import sys
+import zipfile
+import random
 
 #formatting
 RED='\033[0;31m' #red
@@ -9,7 +17,41 @@ BOLD='\033[1m' #bold
 Purple='\033[1;35m' #purple
 Cyan='\033[1;36m' #cyan
 Yellow='\033[1;33m' #yellow
+currentVersion=1.0
 banner = pyfiglet.figlet_format("malScraper", font = "slant")
+
+#Get current user
+def getUser():
+    username = getpass.getuser()
+    return username
+
+#splash-text arrays
+#todo
+
+#time-on-run
+currTime=datetime.now()
+timeStamp=currTime.strftime('%Y-%m-%d-%H:%M')
+#print(timeStamp)
+
+#data locations
+#stores the default location of the download path of the various feeds - these can be modified to custom locations
+PayloadReport = ("C:\\Users\\" + getUser() + "\\Desktop\\malScraper\\PayloadReport.txt")
+C2Report = ("C:\\Users\\" + getUser() + "\\Desktop\\malScraper\\C2Report.txt")
+Top100 = ("C:\\Users\\" + getUser() + "\\Desktop\\malScraper\\Top100Report.txt")
+HexReport = ("C:\\Users\\" + getUser() + "\\Desktop\\malScraper\\HexReport.txt")
+HausMalDown = ("C:\\Users\\" + getUser() + "\\Desktop\\malScraper\\HausMalDown")
+PhishTank = ("C:\\Users\\" + getUser() + "\\Desktop\\malScraper\\PhishTank.csv")
+tempFile = ("C:\\Users\\" + getUser() + "\\Desktop\\malScraper\\temp.zip")
+
+#feed locations
+PayloadFeed=https://urlhaus.abuse.ch/downloads/text/
+C2Feed=cybercrime-tracker.net/all.php
+HexFeed=http://tracker.h3x.eu/api/sites_1month.php
+PhishTank=http://data.phishtank.com/data/online-valid.csv
+#HausMalDown=https://urlhaus.abuse.ch/downloads/csv/
+
+#GitHub CodeLoad API
+release=https://api.github.com/repos/Ryan-Monaghan/malScraper/releases/latest
 
 print(banner)
 
